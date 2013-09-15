9f30931425feb84e8825069a6c11fbd4813a4636
Nebojsa Petrovic <nebspetrovic@gmail.com>
2013-09-15 11:51:20 -0700

    Cleanup formats and styles
    The date formats were cleaned up. Also we show last checkin time for
    users on the floorplan.
    
    Some overall style changes were made.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def show
     @rooms = Room.all
-    @users = User.all.select { |u| u.checkins.count > 0 }
+    @users = User.all.select do |u|
+      u.checkins.count > 0 && u.checkins.last.created_at > 1.days.ago
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def user_photo(user)
     image_tag "http://graph.facebook.com/#{user.uid}/picture?type=small"
   end
+
+  def short_user_name(user)
+    words = user.name.split(' ')
+    result = words.first
+    if words.length > 1
+      result += " #{words[1].capitalize.first}"
+    end
+  end
 end
