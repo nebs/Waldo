ea7d3d8302b0a1a31ffa7f47c1ec615b7d204197
Nebojsa Petrovic <nebspetrovic@gmail.com>
2013-09-29 17:01:12 -0700

    Add facebook dev support
    This commit adds support for Facebook in dev (and prod).

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, AppConfig['FACEBOOK_KEY'], AppConfig['FACEBOOK_SECRET']
 end
