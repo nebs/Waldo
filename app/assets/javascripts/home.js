cba0e0d23a85f59db5f201ef84109659d82abcc5
Nebojsa Petrovic <nebspetrovic@gmail.com>
2013-10-05 08:13:21 -0700

    Fix user click through bug
    There was a bug where if you clicked on a user icon to checkin it would
    not register the room below it.
    
    This is fixed with the pointer-events CSS property

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,32 +1,32 @@
-$(document).ready(function() {
-  var checkin = function(e) {
-    var roomId = $(this).data('roomId');
+var checkin = function(e) {
+  var roomId = $(this).data('roomId');
 
-    var posX = $('.floorplan').offset().left,
-        posY = $('.floorplan').offset().top;
+  var posX = $('.floorplan').offset().left,
+      posY = $('.floorplan').offset().top;
 
-    posX = (e.pageX - posX);
-    posY = (e.pageY - posY);
+  posX = (e.pageX - posX);
+  posY = (e.pageY - posY);
 
-    posX = posX / $('.floorplan').width();
-    posY = posY / $('.floorplan').height();
+  posX = posX / $('.floorplan').width();
+  posY = posY / $('.floorplan').height();
 
-    var options = {
-      x: posX,
-      y: posY
-    };
+  var options = {
+    x: posX,
+    y: posY
+  };
 
-    if (roomId) {
-      options.room_id = roomId
-    }
-
-    $.post('/checkins', options, function() {
-      window.location.href = '/home';
-    });
-
-    return false;
+  if (roomId) {
+    options.room_id = roomId
   }
 
+  $.post('/checkins', options, function() {
+    window.location.href = '/home';
+  });
+
+  return false;
+}
+
+$(document).ready(function() {
   $(document).on('click', '.room', checkin);
   $(document).on('click', '.floorplan', checkin);
 });
