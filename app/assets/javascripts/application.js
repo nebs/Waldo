e1e364058a9ef9f2d53793e08d7602e70c2a6f9e
Nebojsa Petrovic <nebspetrovic@gmail.com>
2013-11-02 20:06:51 -0700

    Remove empty require_tree directory references from app.js
    The application.js file was referencing some empty directories which
    were recently added with the inclusion of backbone.
    
    This was causing problems when trying to deploy to heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,11 +4,7 @@
 //= require turbolinks
 //= require underscore
 //= require backbone
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
 //= require_tree ./views
-//= require_tree ./routers
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
